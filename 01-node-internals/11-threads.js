b2fdf0f97dd4dbea1229a9a5dc20b3c4f0a2bd8f
pcsmomo <dongwoo_333@hanmail.net>
2021-11-26 08:00:05 +1100

    12. Testing for Single Threads

--- 01-node-internals/11-threads.js
+++ 01-node-internals/11-threads.js
@@ -1,6 +1,14 @@
 const crypto = require('crypto');
+const { performance } = require('perf_hooks');
 
-// asynchronous Password-Based Key Derivation Function 2 
+// const start = Date.now();
+const start = performance.now();
+
+// asynchronous Password-Based Key Derivation Function 2
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-  
+  console.log('1:', performance.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('2:', performance.now() - start);
 });
