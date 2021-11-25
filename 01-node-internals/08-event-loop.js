c9504f31b86b7cfb41bc1dc132fc11679beb67d0
pcsmomo <dongwoo_333@hanmail.net>
2021-11-26 07:40:14 +1100

    10. Event Loop Ticks

--- 01-node-internals/08-event-loop.js
+++ 01-node-internals/08-event-loop.js
@@ -9,7 +9,7 @@ const pendingOperations = [];
 // New timers, tasks, operations are recorded from myFile running
 myFile.runContents();
 
-function shouldContinuew() {
+function shouldContinue() {
   // Check one: Any pending setTimeout, setInterval, setImmediate?
   // Check two: Any pending OS tasks? (Like server listening to port)
   // Check three: Any pending long running operations? (Like fs module)
@@ -17,8 +17,22 @@ function shouldContinuew() {
 }
 
 // Entire body executes in one 'tick'
-while (shouldContinuew()) {
+while (shouldContinue()) {
+  // 1) Node looks at pendingTimers and sees if any functions are ready to be called
 
+  // 2) Node looks at pendingOSTasks and pendingOperations and calls relavant callbacks
+
+  // *3) Pause execution. Continue when...
+  //  - a new pendingOSTasks is done
+  //  - a new pendingOperations is done
+  //  - a timer is about to complete
+
+  // 4) Look at pendingTimers. Call any setImmediate
+
+  // 5) Handle any 'close' events
+  // readStream.on('close', () => {
+  //   console.log('Cleanup code')
+  // })
 }
 
 // exit back to terminal
