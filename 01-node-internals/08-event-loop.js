663efbcd44e4bfc929e9aec9317227d7d722d2c6
pcsmomo <dongwoo_333@hanmail.net>
2021-11-26 07:34:16 +1100

    9. The Event Loop Implementation

--- 01-node-internals/08-event-loop.js
+++ 01-node-internals/08-event-loop.js
@@ -2,10 +2,18 @@
 
 // node myFile.js
 
+const pendingTimers = [];
+const pendingOSTasks = [];
+const pendingOperations = [];
+
+// New timers, tasks, operations are recorded from myFile running
 myFile.runContents();
 
 function shouldContinuew() {
-  
+  // Check one: Any pending setTimeout, setInterval, setImmediate?
+  // Check two: Any pending OS tasks? (Like server listening to port)
+  // Check three: Any pending long running operations? (Like fs module)
+  return pendingTimers.length || pendingOSTasks.length || pendingOperations.length
 }
 
 // Entire body executes in one 'tick'
