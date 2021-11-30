11aeab888bc09b369aeb07b709dc9141a6859f88
pcsmomo <dongwoo_333@hanmail.net>
2021-12-01 06:24:31 +1100

    66. Cache Expiration

--- 04-redis-data-caching/blog/services/cache.js
+++ 04-redis-data-caching/blog/services/cache.js
@@ -44,7 +44,7 @@ mongoose.Query.prototype.exec = async function () {
   const result = await exec.apply(this, arguments);
 
   console.log(`${this.mongooseCollection.name} - SERVING FROM MONGODB`);
-  client.set(key, JSON.stringify(result));
+  client.set(key, JSON.stringify(result), 'EX', 10);
 
   return result;
 };
