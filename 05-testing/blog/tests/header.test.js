c9ddf1ce851b604aa1209da098d84347d713bbce
pcsmomo <dongwoo_333@hanmail.net>
2021-12-04 07:09:53 +1100

    104. Custom Page Implementation

--- 05-testing/blog/tests/header.test.js
+++ 05-testing/blog/tests/header.test.js
@@ -1,19 +1,16 @@
-const puppeteer = require('puppeteer');
+const Page = require('./helpers/page');
 const sessionFactory = require('./factories/sessionFactory');
 const userFactory = require('./factories/userFactory');
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false
-  });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test('the header has the correct text', async () => {
