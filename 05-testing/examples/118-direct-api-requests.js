1bc72c07bde52aab66f41f091c84975d481551af
pcsmomo <dongwoo_333@hanmail.net>
2021-12-04 18:29:28 +1100

    121. Get Restrictions

--- 05-testing/examples/118-direct-api-requests.js
+++ 05-testing/examples/118-direct-api-requests.js
@@ -1,5 +1,6 @@
 // Temporary file to create a new blog post
 
+// POST
 () => {
   fetch('/api/blogs', {
     method: 'POST',
@@ -9,4 +10,15 @@
     },
     body: JSON.stringify({ title: 'My Title', content: 'My Content' })
   }).then(res => res.json());
+};
+
+// Get
+() => {
+  fetch('/api/blogs', {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 };
