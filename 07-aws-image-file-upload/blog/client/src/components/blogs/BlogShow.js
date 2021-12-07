64fffdd94005f6284e160cb706240e2b848eedc4
pcsmomo <dongwoo_333@hanmail.net>
2021-12-07 17:53:42 +1100

    167. Displaying Images

--- 07-aws-image-file-upload/blog/client/src/components/blogs/BlogShow.js
+++ 07-aws-image-file-upload/blog/client/src/components/blogs/BlogShow.js
@@ -7,6 +7,17 @@ class BlogShow extends Component {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    console.log(this.props.blog);
+    if (this.props.blog.imageUrl) {
+      return (
+        <img
+          src={`https://blogster-bucket-noah.s3.ap-southeast-2.amazonaws.com/${this.props.blog.imageUrl}`}
+        />
+      );
+    }
+  }
+
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +29,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
